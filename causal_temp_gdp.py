a1696d40cd3f1e10f869509a7bc39619c70d4df7
luisicart <icartenrique@gmail.com>
2025-06-23 20:42:23 -0300

    adding Variavle Plot

--- causal_temp_gdp.py
+++ causal_temp_gdp.py
@@ -2,14 +2,22 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import statsmodels.api as sm
+import seaborn as sns
 import statsmodels.formula.api as smf
 from statsmodels.graphics.regressionplots import plot_partregress, plot_partregress_grid
-
+# %%
+plt.rcParams.update({
+        'axes.grid': False,  
+        'axes.linewidth': 1.5,  
+        'axes.edgecolor': 'black',  
+        'axes.facecolor': 'white',  
+        'figure.facecolor': 'white',  
+        'axes.spines.top': False,  
+        'axes.spines.right': False
+    })
 # %%
 temp_df = pd.read_csv('./data/average_surface_temperature.csv')
 gdp_df = pd.read_csv('./data/gdp_per_capita_worldbank.csv')
-
 # %%
 common_rename_columns = {
     'Entity': 'country',
@@ -28,20 +36,9 @@ gdp_specific_rename = {
 
 temp_df.rename(columns={**common_rename_columns, **temp_specific_rename}, inplace=True)
 gdp_df.rename(columns={**common_rename_columns, **gdp_specific_rename}, inplace=True)
-
 # %%
 def filter_valid_country_codes(df, code_col, length=3):
     return df[df[code_col].str.len() == length]
-
-def create_avplot(yvar, xvar, other_vars, df, title, filename,
-                  figsize=(10, 6), dpi=300):
-    fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
-    plot_partregress(yvar, xvar, other_vars, data=df, ax=ax, obs_labels=False)
-    ax.set_title(title)
-    fig.tight_layout()
-    fig.savefig(filename)
-    plt.show() 
-
 # %%
 temp_df = filter_valid_country_codes(temp_df, 'country_code')
 gdp_df = filter_valid_country_codes(gdp_df, 'country_code')
@@ -55,22 +52,57 @@ gdp_df['ln_gdp'] = np.log(gdp_df['gdp'])
 temp_gdp_df = pd.merge(gdp_df, temp_df, on=['country_code', 'year'], how='inner')
 temp_gdp_df
 # %%
-# Regressão 1: ln_gdp ~ temp (robusto)
-modelo1 = smf.ols(formula='ln_gdp ~ average_annual_temperature', data=temp_gdp_df[temp_gdp_df['year'] == 2020]).fit(cov_type='HC1')
-print("\nRegressão 1: ln_gdp ~ temp (2020)")
-print(modelo1.summary())
+selected_year = 2020
+temp_gdp_df = temp_gdp_df[temp_gdp_df['year'] == selected_year]
 
 # %%
-# a) avplot para modelo1 (só temp)
-create_avplot(
-    yvar='ln_gdp',
-    xvar='average_annual_temperature',
-    other_vars=[],
-    df=temp_gdp_df[temp_gdp_df['year'] == 2020],
-    title='Added Variable Plot - Temperatura',
-    filename='./graphs/gdp_temperatura_python.png'
+top_5_countries = temp_gdp_df.nlargest(5, 'gdp')['country_code'].tolist()
+bottom_5_countries = temp_gdp_df.nsmallest(4, 'gdp')['country_code'].tolist()
+
+top_and_bottom_countries = top_5_countries + bottom_5_countries
+# %%
+plt.figure(figsize=(10, 6), dpi=300)
+sns.scatterplot(
+    data=temp_gdp_df,
+    x='average_annual_temperature',
+    y='ln_gdp',
+    size='ln_gdp',
+    hue='ln_gdp',
+    palette='RdBu'
 )
 
+for i, row in temp_gdp_df.iterrows():
+    if row['country_code'] in top_and_bottom_countries:
+        plt.text(
+            row['average_annual_temperature'],
+            row['ln_gdp'] + 0.05,  
+            row['country'],   
+            fontsize=8,
+            ha='center',
+            va='bottom'
+        )
+
+sns.regplot(
+    data=temp_gdp_df,
+    x='average_annual_temperature',
+    y='ln_gdp',
+    ci=95,
+    scatter=False,
+    color='black',
+    line_kws={'label': 'Regressão Linear'}
+)
+plt.title(f'Relação entre Temperatura e PIB per Capita (ln) - {selected_year}')
+plt.xlabel('Temperatura Anual Média (°C)')
+plt.ylabel('PIB per Capita (ln)')
+plt.savefig('./graphs/gdp_vs_temperatura.png')
+# %%
+# ln_gdp ~ temp
+reg_model_temp_gdp = smf.ols(formula='ln_gdp ~ average_annual_temperature', data=temp_gdp_df).fit(cov_type='HC1')
+print(f"\nRegressão: ln_gdp ~ temp {selected_year}")
+print(reg_model_temp_gdp.summary())
+# %%
+ate_simple = reg_model_temp_gdp.params['average_annual_temperature']
+print(f"\nATE (modelo simples, sem controles): {ate_simple:.4f}")
 # %%
 industry_df = pd.read_csv("./data/industry_share_of_total_emplyoment.csv")
 
@@ -80,9 +112,7 @@ industry_specific_rename = {
 
 industry_df.rename(columns={**common_rename_columns, **industry_specific_rename}, inplace=True)
 industry_df = filter_valid_country_codes(industry_df, 'country_code')
-
 # %%
-
 educ_df = pd.read_csv("./data/mean_years_of_schooling_long_run.csv")
 
 educ_specific_rename = {
@@ -106,26 +136,36 @@ df_confounders = temp_gdp_df.copy()
 for df in [industry_df, educ_df, rule_law_df]:
     df_confounders = pd.merge(df_confounders, df.drop(columns='country'), on=['country_code', 'year'], how='inner')
 
+df_confounders = df_confounders[df_confounders['year'] == selected_year]
 # %%
-# Regressão 2: ln_gdp ~ temp + educ + rule_law + industry (robusto)
-modelo2 = smf.ols(formula='ln_gdp ~ average_annual_temperature + educ_level + law_level + industry_level', 
-                  data=df_confounders[df_confounders['year'] == 2020]).fit(cov_type='HC1')
-print("\nRegressão 2: ln_gdp ~ temp + educ + law + industry")
-print(modelo2.summary())
+# ln_gdp ~ temp + educ + rule_law + industry 
+reg_model_confounders = smf.ols(formula='ln_gdp ~ average_annual_temperature + educ_level + law_level + industry_level', 
+                  data=df_confounders).fit(cov_type='HC1')
+print(f"\nRegressão: ln_gdp ~ temp + educ + law + industry {selected_year}")
+print(reg_model_confounders.summary())
 
 # %%
-create_avplot(
-    yvar='ln_gdp',
-    xvar='average_annual_temperature',
-    other_vars=['educ_level', 'law_level', 'industry_level'],
-    df=df_confounders[df_confounders['year'] == 2020],
-    title='Added Variable Plot - Temperatura (Condicionado)',
-    filename='./graphs/gdp_temperatura_condicional_python.png'
-)
+ate_adjusted = reg_model_confounders.params['average_annual_temperature']
+print(f"\nATE (modelo com controles - confounders): {ate_adjusted:.4f}")
+# %%
+fig = plt.figure(figsize=(10, 6), dpi=300)
+ax = plt.gca()
+plot_partregress(
+        endog='ln_gdp', 
+        exog_i='average_annual_temperature', 
+        exog_others=['educ_level', 'law_level', 'industry_level'], 
+        data=df_confounders, 
+        obs_labels=False,
+        ax=ax)
 
+ax.set_title('Added Variable Plot - Temperatura Condicional')
+fig.tight_layout()
+fig.savefig('./graphs/gdp_temperatura_condicional.png')
+plt.show()
 # %%
 fig = plt.figure(figsize=(10, 8), dpi=300)
-plot_partregress_grid(modelo2, fig=fig)
+axes = plot_partregress_grid(reg_model_confounders, fig=fig)
+fig.suptitle('Gráficos de Regressão Parcial - Condicionais')
 fig.tight_layout()
-fig.savefig('./graphs/gdp_controles_python.png')
-plt.show() 
+fig.savefig('./graphs/gdp_confounders.png')
+plt.show()
